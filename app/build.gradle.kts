cf7845216fa90f66744107ac0956d26ebcb401c5
Kunal <ITKunal86@hotmail.com>
2025-12-16 11:55:06 +0530

    update stack trace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.hirenq.tmmrelay"
+    namespace = "com.example.tmmrelay"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.hirenq.tmmrelay"
+        applicationId = "com.example.tmmrelay"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
